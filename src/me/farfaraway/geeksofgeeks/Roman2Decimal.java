cdf9bd935a95baca39d234bb1ef9987ad7b0fd0e
Farhan Faruque <russel.cse05@gmail.com>
2017-02-12 01:51:03 +0600

    fixed untracked files

--- src/me/farfaraway/geeksofgeeks/Roman2Decimal.java
+++
@@ -1,7 +0,0 @@
-package me.farfaraway.geeksofgeeks;
-
-/**
- * http://www.geeksforgeeks.org/converting-roman-numerals-decimal-lying-1-3999/
- */
-public class Roman2Decimal {
-}
--- src/me/farfaraway/geeksofgeeks/hacker.java
+++
@@ -1,7 +0,0 @@
-package me.farfaraway.geeksofgeeks;
-
-/**
- * Created by farhan on 2/12/17.
- */
-public class hacker {
-}
