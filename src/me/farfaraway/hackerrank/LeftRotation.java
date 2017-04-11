d85d81c16593484fc819d0c725f89a178f02e069
Farhan Faruque <russel.cse05@gmail.com>
2017-04-11 23:26:55 +0600

    linked list solution

---
+++ src/me/farfaraway/hackerrank/LeftRotation.java
@@ -0,0 +1,37 @@
+package me.farfaraway.hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/array-left-rotation
+ */
+public class LeftRotation {
+
+
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        int numberOfItems = in.nextInt();
+        int rolationLenght = in.nextInt();
+
+        int [] array = new int[numberOfItems];
+        for (int i = 0; i<numberOfItems; i++){
+            array[i] = in.nextInt();
+        }
+
+        for (int i = 0; i< rolationLenght ; i++) {
+            if (array.length == 0 )
+                return;
+
+            int j = 0;
+            int temp = array [j];
+            for (j = 0; j< array.length; j++) {
+                if (j != array.length -1)
+                    array[j] = array[j + 1];
+            }
+            array[array.length -1] = temp;
+        }
+        for (int j = 0; j< array.length; j++)
+        System.out.print(array[j] + " ");
+    }
+
+}
