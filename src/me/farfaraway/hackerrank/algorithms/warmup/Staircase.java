c80c443c5e8010e2e883c23654dd132ddc7d3d35
Farhan Faruque <russel.cse05@gmail.com>
2017-04-12 15:44:52 +0600

    Birthday cake candle

--- src/me/farfaraway/hackerrank/algorithms/warmup/Staircase.java
+++ src/me/farfaraway/hackerrank/algorithms/warmup/Staircase.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
  */
 public class Staircase {
 
-  public static void main(String [] args){
+  public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
     stair(n);
 
   }
 
-  public static void stair(final int n){
-    for (int i = 1; i<=n; i++){
+  public static void stair(final int n) {
+    for (int i = 1; i <= n; i++) {
 
-      for (int j = 1; j<= (n - i); j++){
+      for (int j = 1; j <= (n - i); j++) {
         System.out.print(" ");
       }
 
-      for (int j = 1; j<=i; j++){
+      for (int j = 1; j <= i; j++) {
         System.out.print("#");
       }
 
