7e67e76003aa163641ff9dbadd08b4395768d0cc
Farhan Faruque <russel.cse05@gmail.com>
2017-04-12 15:24:35 +0600

    Add algorithms code

---
+++ src/me/farfaraway/hackerrank/algorithms/warmup/DiagonalDifference.java
@@ -0,0 +1,8 @@
+package me.farfaraway.hackerrank.algorithms.warmup;
+
+/**
+ * https://www.hackerrank.com/challenges/diagonal-difference
+ */
+
+public class DiagonalDifference {
+}
---
+++ src/me/farfaraway/hackerrank/algorithms/warmup/Staircase.java
@@ -0,0 +1,33 @@
+package me.farfaraway.hackerrank.algorithms.warmup;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/staircase
+ */
+public class Staircase {
+
+  public static void main(String [] args){
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    stair(n);
+
+  }
+
+  public static void stair(final int n){
+    for (int i = 1; i<=n; i++){
+
+      for (int j = 1; j<= (n - i); j++){
+        System.out.print(" ");
+      }
+
+      for (int j = 1; j<=i; j++){
+        System.out.print("#");
+      }
+
+      System.out.print("\n");
+
+    }
+  }
+
+}
