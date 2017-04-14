14842de879314ac09e04851ab6aea9c6fa0913af
Farhan Faruque <russel.cse05@gmail.com>
2017-04-15 00:56:31 +0600

    Diagonal

---
+++ src/me/farfaraway/hackerrank/algorithms/strings/ReducedString.java
@@ -0,0 +1,12 @@
+package me.farfaraway.hackerrank.algorithms.strings;
+
+/**
+ * Created by farhan on 4/12/17.
+ * https://www.hackerrank.com/challenges/reduced-string
+ */
+public class ReducedString {
+
+  public static void main(String [] args){
+
+  }
+}
